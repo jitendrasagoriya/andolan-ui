6a2635347c581ba664d80eedabf0cc88b9cc9ced
jitendrasagoriya <jitendrasagoriya83@gmail.com>
2019-03-08 22:21:58 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './component/registration/registration.component';
+import { HomeComponent } from './component/home/home.component';
 
+
+const appRoutes: Routes = [
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
